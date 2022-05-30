6fb405135e53bd9fae0bfb1498fab89c38f63ebb
ainsyakina20 <100009907+ainsyakina20@users.noreply.github.com>
2022-05-30 12:21:57 +0800

    Add files via upload

---
+++ v.2/src/penerimaanbeasiswa/PenerimaanBeasiswa.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penerimaanbeasiswa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author ahmadkurniawan
+ */
+public class PenerimaanBeasiswa {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private static Connection MYSQLConfig;
+    
+    public static Connection ConfigDB() throws SQLException{
+        
+        try{
+            
+            String url  = "jdbc:mysql://localhost:3306/beasiswa";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MYSQLConfig = DriverManager.getConnection(url,user,pass);
+            
+        }catch(SQLException e){
+            
+            System.out.println("Koneksi Database Gagal !!!" + e.getMessage());
+            
+        }
+        
+        return MYSQLConfig;
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
